9a397eab3792e045ae0a229a5c2090d16c387095
Allan Jensen <info@winternet.no>
2016-08-12 11:25:57 +0000

    Added option to set JPG iamge quality and increased the default quality

--- src/pdf.php
+++ src/pdf.php
@@ -16,6 +16,7 @@ class pdf {
 		$defaults = array(
 			'output_format' => 'jpg',  //nothing else has currently been implemented
 			'resolution' => 150,
+			'jpg_quality' => 90,
 			'suffix_pattern' => '-%d',  //suffix added to file name for each page of the converted PDF (%d indicates page number)
 			'ghostscript_path' => 'gs',
 		);
@@ -27,7 +28,7 @@ class pdf {
 
 		$sdevice = (in_array($options['output_format'], array('jpg', 'jpeg')) ? 'jpeg' : '');
 
-		exec($options['ghostscript_path'] .' -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE='. $sdevice .' -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r'. $options['resolution'] .'  -sOutputFile='. $image_path .' '. $pdf_path .' 2>&1', $coutput, $returncode);
+		exec($options['ghostscript_path'] .' -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE='. $sdevice .' -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r'. $options['resolution'] .' -dJPEGQ='. $options['jpg_quality'] .' -sOutputFile='. $image_path .' '. $pdf_path .' 2>&1', $coutput, $returncode);
 		if (!empty($coutput)) {
 			throw new \Exception("Error in ". __FILE__ .":". __LINE__ ." -- GhostScript command for converting PDF to image outputted unexpected data ((INTERNAL:". json_encode($coutput) ."))");
 		} elseif ($returncode != 0) {
