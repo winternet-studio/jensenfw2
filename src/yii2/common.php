6dcbdc8000bae9de5777f52b3765101a49112b6a
Allan Jensen <info@winternet.no>
2016-12-14 13:31:22 +0000

    Added common Yii class to JensenFW2

---
+++ src/yii2/common.php
@@ -0,0 +1,43 @@
+<?php
+namespace winternet\jensenfw2\yii;
+
+class common {
+	public static function add_result_errors($result, &$model, $options = []) {
+		/*
+		DESCRIPTION:
+		- add errors from a Yii model to a standard result array
+		- the new key 'err_msg_ext' MUST then be used for processing it (because 'err_msg' might not contain all error messages)
+		INPUT:
+		- $result : empty variable (null, false, whatever) or an associative array in this format: ['status' => 'ok|error', 'result_msg' => [], 'err_msg' => []]
+		- $model : a Yii model
+		- $options : associative array with any of these keys: 
+			- 'add_existing' : add the existing 'err_msg' array entries to 'err_msg_ext'
+		OUTPUT:
+		- associative array in the format of $result but with the new key 'err_msg_ext'
+		*/
+		if (!is_array($result)) {
+			$result = [
+				'status' => 'ok',
+				'result_msg' => [],
+				'err_msg' => [],
+			];
+		}
+
+		$model_errors = $model->getErrors();
+
+		$result['err_msg_ext'] = $model_errors;
+
+		if ($options['add_existing']) {
+			if (!empty($result['err_msg'])) {
+				$result['err_msg_ext']['_global'] = $result['err_msg'];
+			}
+		}
+
+		// Ensure correct status
+		if (!empty($result['err_msg'])) {
+			$result['status'] = 'error';
+		}
+
+		return $result;
+	}
+}
