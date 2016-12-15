4795873050b184e33d1a118ee0f7dc91878243fe
Allan Jensen <info@winternet.no>
2016-12-15 16:57:16 +0000

    Added process_ajax_submit()

--- src/yii2/common.php
+++ src/yii2/common.php
@@ -2,6 +2,40 @@
 namespace winternet\jensenfw2\yii2;
 
 class common {
+	public static function process_ajax_submit($options = []) {
+		/*
+		DESCRIPTION:
+		- 
+		INPUT:
+		- $options : associative array with any of these keys:
+			- 'form' : ActiveForm object
+			- 'on_error' : name of callback function when submission caused some errors
+			- 'on_success' : name of callback function when submission succeeded
+			- 'on_complete' : name of callback function that will always be called
+		OUTPUT:
+		- 
+		*/
+		$js = "function(rsp) {";
+		if ($options['form']) {
+			// Apply the server-side generated errors to the form fields
+			$js .= "var form = $(_clickedButton).parents('form');
+if (typeof rsp.err_msg_ext != 'undefined') { form.yiiActiveForm('updateMessages', rsp.err_msg_ext); }
+var errorCount = form.find('.has-error').length;";
+		} else {
+			$js .= "var form, errorCount;";
+			$js .= "if (rsp.err_msg) errorCount = rsp.err_msg.length;";
+		}
+
+		if ($options['on_error'] || $options['on_success']) {
+			$js .= "if (errorCount > 0) {". ($options['on_error'] ? $options['on_error'] .'({form:form, rsp:rsp, errorCount:errorCount});' : '') ."} else {". ($options['on_success'] ? $options['on_success'] .'({form:form, rsp:rsp, errorCount:errorCount});' : '') ."}";
+		}
+		if ($options['on_complete']) {
+			$js .= $options['on_complete'] .'({form:form, rsp:rsp, errorCount:errorCount});';
+		}
+		$js .= "}";
+		return new \yii\web\JsExpression($js);
+	}
+
 	public static function add_result_errors($result, &$model, $options = []) {
 		/*
 		DESCRIPTION:
@@ -25,7 +59,16 @@ class common {
 
 		$model_errors = $model->getErrors();
 
-		$result['err_msg_ext'] = $model_errors;
+		foreach ($model_errors as $attr => $errors) {
+			// Generate the form field ID so Yii ActiveForm client-side can apply the error message
+			if (!$model_name) {
+				$model_name = $model::className();
+				$model_name = mb_strtolower(substr($model_name, strrpos($model_name, '\\')+1));
+			}
+
+			$result['err_msg_ext'][$model_name .'-'. $attr] = array_merge($errors, $errors);
+		}
+
 
 		if ($options['add_existing']) {
 			if (!empty($result['err_msg'])) {
