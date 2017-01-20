84481fe20630634df2de226963a2b441faa02271
Allan Jensen <info@winternet.no>
2017-01-20 11:35:19 +0000

    Added support for converting to PNG (with transparency)

--- src/pdf.php
+++ src/pdf.php
@@ -10,11 +10,12 @@ class pdf {
 		- $pdf_path : full path and filename of the PDF to convert
 		- $image_path : full path and filename of the image to write to
 						If PDF has multiple pages insert %d where you want to insert page number into the destination filename
+		- $options : see below (in the code)
 		OUTPUT:
 		- nothing
 		*/
 		$defaults = array(
-			'output_format' => 'jpg',  //nothing else has currently been implemented
+			'output_format' => 'jpg',  //'jpg' or 'png'
 			'resolution' => 150,
 			'jpg_quality' => 90,
 			'suffix_pattern' => '-%d',  //suffix added to file name for each page of the converted PDF (%d indicates page number)
@@ -26,9 +27,17 @@ class pdf {
 
 		$options = array_merge($defaults, (array) $options);
 
-		$sdevice = (in_array($options['output_format'], array('jpg', 'jpeg')) ? 'jpeg' : '');
+		if ($options['output_format'] == 'jpg' || $options['output_format'] == 'jpeg') {
+			$sdevice = 'jpeg';
+		} elseif ($options['output_format'] == 'png') {
+			$sdevice = 'pngalpha';
+		} else {
+			core::system_error('Invalid output format for converting PDF to image.', ['Format' => $options['output_format']]);
+		}
 
-		exec($options['ghostscript_path'] .' -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE='. $sdevice .' -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r'. $options['resolution'] .' -dJPEGQ='. $options['jpg_quality'] .' -sOutputFile='. $image_path .' '. $pdf_path .' 2>&1', $coutput, $returncode);
+		$cmd = $options['ghostscript_path'] .' -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE='. $sdevice .' -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r'. $options['resolution'] . ($options['output_format'] == 'jpg' ? ' -dJPEGQ='. $options['jpg_quality'] : '') .' -sOutputFile='. $image_path .' '. $pdf_path .' 2>&1';
+		exec($cmd, $coutput, $returncode);
+
 		if (!empty($coutput)) {
 			throw new \Exception("Error in ". __FILE__ .":". __LINE__ ." -- GhostScript command for converting PDF to image outputted unexpected data ((INTERNAL:". json_encode($coutput) ."))");
 		} elseif ($returncode != 0) {
