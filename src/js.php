387be6a65a725b0b61925c2c3a1db6feac2b70aa
Allan Jensen <info@winternet.no>
2017-04-27 14:06:01 +0000

    Made it possible update values from pass_to_js() / getphp()

--- src/js.php
+++ src/js.php
@@ -5,6 +5,9 @@ This file contains general Javascript formatting functions
 namespace winternet\jensenfw2;
 
 class js {
+
+	private static $pass_to_js_counter = 0;
+
 	static public function esc($str) {
 		/*
 		DESCRIPTION:
@@ -23,18 +26,22 @@ class js {
 		DESCRIPTION:
 		- function to pass information (mainly translations of text) from PHP to Javascript when it's placed in a separate file
 		- call getphp(ref) in Javascript to get the information from PHP, where ref is the key from the array provided to this function
+		- changes to the values will be retained
 		INPUT:
 		- $array : associate array where keys are the references that will later be used in Javascript to retrieve the corresponding values
 			- for translation system use the translation tag as the key and set to value to '_txt' in order for this function to retrieve the translation itself
 		- $options : associative array with any of these flags:
 			- 'js_function_name' : use a different name for the Javascript function. Default is 'getphp'
 			- 'is_js_context' : set to true when the output is placed directly in a Javascript block (= skip the <script> tags)
+			- 'storage_var_prefix' : name of variable where the information will be stored. A number will be appended to the string.
+				- if only a local variable is used it will not be possible to change values, will effectively be read-only
 		OUTPUT:
 		- writes a block of Javascript to the browser
 		*/
 		$default_options = array(
 			'js_function_name' => 'getphp',
 			'is_js_context' => false,
+			'storage_var_prefix' => 'window.jfwJsV',
 		);
 		$options = array_merge($default_options, (array) $options);
 
@@ -53,11 +60,15 @@ class js {
 			$out  = "<script type=\"text/javascript\">\r\n";
 			$out .= "/* <![CDATA[ */\r\n";
 		}
+
+		self::$pass_to_js_counter++;
+		$varname = $options['storage_var_prefix'] . self::$pass_to_js_counter;
+
 		$out .= "function ". $options['js_function_name'] ."(ref){\r\n";  //default: function getphp()
-		$out .= "var v=". json_encode($a) .";\r\n";
-		$out .= "if(typeof ref==\"undefined\"){return v};\r\n";
-		$out .= "if(typeof v[ref]==\"undefined\"){alert(\"Configuration error. The reference '\"+ ref +\"' does not exist.\");return\"UNKNOWN_REFERENCE:\"+ref}";
-		$out .= "return v[ref];";
+		$out .= "if(typeof ". $varname ."=='undefined')". $varname ."=". json_encode($a) .";\r\n";
+		$out .= "if(typeof ref==\"undefined\"){return ". $varname ."};\r\n";
+		$out .= "if(typeof ". $varname ."[ref]==\"undefined\"){alert(\"Configuration error. The reference '\"+ ref +\"' does not exist.\");return\"UNKNOWN_REFERENCE:\"+ref}";
+		$out .= "return ". $varname ."[ref];";
 		$out .= "}\r\n";
 		if (!$options['is_js_context']) {
 			$out .= "/* ]]> */\r\n";
