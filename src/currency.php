64f988c41ef884cb0b1cdf4281a72601171ce348
Allan Jensen <info@winternet.no>
2018-09-10 09:40:34 +0000

    get_ecb_live_exchange_rate(): changed to https and follow redirects and handle exceptions

--- src/currency.php
+++ src/currency.php
@@ -27,28 +27,34 @@ class currency {
 
 		if (!$session_value) {
 			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
+			curl_setopt($ch, CURLOPT_URL, 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 			$xmlStr = curl_exec($ch);
-			$xml = simplexml_load_string($xmlStr);
-			if ($xml === false) {
-				//use default below
-			} else {
-				$from_rate = $to_rate = false;
-				if ($from_currency == 'EUR') $from_rate = 1;
-				if ($to_currency == 'EUR') $to_rate = 1;
-				$json = json_decode(json_encode($xml), true);
-				foreach ($json['Cube']['Cube']['Cube'] as $curr) {
-					if ($curr['@attributes']['currency'] == $from_currency) {
-						$from_rate = $curr['@attributes']['rate'];
-					} elseif ($curr['@attributes']['currency'] == $to_currency) {
-						$to_rate = $curr['@attributes']['rate'];
+			try {
+				$xml = simplexml_load_string($xmlStr);
+				if ($xml === false) {
+					//use fallback below
+				} else {
+					$from_rate = $to_rate = false;
+					if ($from_currency == 'EUR') $from_rate = 1;
+					if ($to_currency == 'EUR') $to_rate = 1;
+					$json = json_decode(json_encode($xml), true);
+					foreach ($json['Cube']['Cube']['Cube'] as $curr) {
+						if ($curr['@attributes']['currency'] == $from_currency) {
+							$from_rate = $curr['@attributes']['rate'];
+						} elseif ($curr['@attributes']['currency'] == $to_currency) {
+							$to_rate = $curr['@attributes']['rate'];
+						}
 					}
 				}
+				if ($from_rate && $to_rate && is_numeric($from_rate) && is_numeric($to_rate)) {
+					$exchrate = $from_rate / $to_rate;
+				}
+			} catch (\Exception $e) {
+				//use fallback below
 			}
-			if ($from_rate && $to_rate && is_numeric($from_rate) && is_numeric($to_rate)) {
-				$exchrate = $from_rate / $to_rate;
-			}
+
 			if (!$exchrate) {
 				$exchrate = $fallback_rate;
 			}
