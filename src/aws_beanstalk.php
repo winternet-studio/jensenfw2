cce9bac61175f9dff9e89c8030c90a4fe4a3d721
Allan Jensen <info@winternet.no>
2018-12-11 18:18:22 +0000

    Implemented get_environment_url()

--- src/aws_beanstalk.php
+++ src/aws_beanstalk.php
@@ -72,11 +72,52 @@ class aws_beanstalk {
 	/**
 	 * Get public URL for the current environment
 	 *
+	 * Must be run as root if $region or $environmentID is not provided.
+	 *
 	 * @return string : For example `http://myserver.eu-west-1.elasticbeanstalk.com/`
 	 */
-	public function get_environment_url() {
-		// WATCH: https://stackoverflow.com/questions/45907623/is-it-possible-to-get-elastic-beanstalk-address-from-within-its-ec2-instance
-		core::error('Havent found a solution yet to getting the environment URL.');
+	public function get_environment_url($region = null, $environmentID = null) {
+		// Source: https://serverfault.com/a/943320/340429
+		// Similar: https://stackoverflow.com/questions/45907623/is-it-possible-to-get-elastic-beanstalk-address-from-within-its-ec2-instance
+	
+		if (!$region || !$environmentID) {
+			$file = '/etc/elasticbeanstalk/.aws-eb-stack.properties';
+			$props = file_get_contents($file);
+			if (!$props) {
+				core::system_error('Failed to read AWS Elastic Beanstalk stack properties.', ['Details' => 'Probably missing permission. Only root can read it.']);
+			}
+		}
+
+		if (!$region) {
+			if (preg_match("/^region=([a-z0-9\\-\\s]+)$/mi", $props, $match)) {
+				$region = trim($match[1]);
+			} else {
+				core::system_error('Failed to find region in AWS Elastic Beanstalk stack properties.', ['Props' => $props]);
+			}
+		} else {
+			$region = preg_replace("/[^a-z0-9\\-]/", '', $region);
+		}
+
+		if (!$environmentID) {
+			if (preg_match("/^environment_id=([a-z0-9\\-\\s]+)$/mi", $props, $match)) {
+				$environmentID = trim($match[1]);
+			} else {
+				core::system_error('Failed to find environment ID in AWS Elastic Beanstalk stack properties.', ['Props' => $props]);
+			}
+		} else {
+			$environmentID = preg_replace("/[^a-z0-9\\-]/", '', $environmentID);
+		}
+
+		$cmd = 'aws elasticbeanstalk describe-environments --region '. $region .' --environment-id '. $environmentID .' --query "Environments[0].CNAME"';
+
+		$output = []; $exitcode = 0;
+		exec($cmd, $output, $exitcode);
+		
+		if ($exitcode != 0) {
+			core::system_error('Getting environment URL resulted in exit code '. $exitcode .'.', ['Output' => $output]);
+		}
+
+		return trim(trim(implode('', $output)), '"');
 	}
 
 
