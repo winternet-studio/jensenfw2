552402604856670361114269f8e0a7f91330ccac
Allan Jensen <info@winternet.no>
2019-07-24 15:49:52 +0000

    convert_pdf_to_image(): implemented use_box=ArtBox for xpdf engine

--- src/pdf.php
+++ src/pdf.php
@@ -39,6 +39,7 @@ class pdf {
 			}
 			$cmd .= ' -r '. $options['resolution'];
 			$cmd .= ' '. escapeshellarg($pdf_path) .' '. escapeshellarg($image_path) .' 2>&1';
+			$coutput = array();
 			exec($cmd, $coutput, $returncode);  //exit codes: https://www.xpdfreader.com/pdftopng-man.html
 
 			if (!empty($coutput)) {
@@ -73,6 +74,7 @@ class pdf {
 			$image_path_safe = escapeshellarg($image_path_safe);
 			$image_path_safe = str_replace('-PERCENTAGE-d', '%d', $image_path_safe);
 			$cmd = $options['ghostscript_path'] .' -q -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT '. $additional_options .' -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -sDEVICE='. $sdevice .' -dTextAlphaBits='. $options['TextAlphaBits'] .' -dGraphicsAlphaBits='. $options['GraphicsAlphaBits'] .' -r'. $options['resolution'] . ($options['output_format'] == 'jpg' ? ' -dJPEGQ='. $options['jpg_quality'] : '') .' -sOutputFile='. $image_path_safe .' '. escapeshellarg($pdf_path) .' 2>&1';
+			$coutput = array();
 			exec($cmd, $coutput, $returncode);
 
 			if (!empty($coutput)) {
@@ -82,24 +84,105 @@ class pdf {
 			}
 		}
 
-		if ($options['engine'] === 'xpdf' && $options['output_format'] == 'jpg') {
-			// xpdf doesn't support output in jpg so in that case we do this fill-in using ImageMagick to support that
+		if ($options['engine'] === 'xpdf' && ($options['output_format'] === 'jpg' || $options['use_box'] === 'ArtBox')) {
+
+			// xpdf doesn't support only extracting the ArtBox so do this fill-in by using pdfinfo to get the details and crop the image
+			if ($options['use_box'] === 'ArtBox') {
+				// Get the info about boxes as well as page size
+				if (!$options['xpdf_info_path']) {  //if the unofficial option has not been passed then just derive it from the `pdftopng` path
+					$options['xpdf_info_path'] = str_replace('pdftopng', 'pdfinfo', $options['xpdf_path']);
+				}
+				$cmd = $options['xpdf_info_path'] .' -box '. escapeshellarg($pdf_path) .' 2>&1';
+				$coutput = array();
+				exec($cmd, $coutput, $returncode);
+
+				if (empty($coutput)) {
+					throw new \Exception('xpdf pdfinfo command for getting PDF boxes outputted unexpected data ((INTERNAL:'. json_encode($coutput) .'))');
+				} elseif ($returncode != 0) {
+					throw new \Exception('xpdf pdfinfo command for getting PDF boxes returned '. $returncode);
+				}
+
+				$pagesize_info = $artbox_info = null;
+				foreach ($coutput as $cline) {
+					if (strtolower(substr($cline, 0, 10)) == 'page size:') {  //pdfinfo documentation: https://www.xpdfreader.com/pdfinfo-man.html
+						$pagesize_info = $cline;
+					} elseif (strtolower(substr($cline, 0, 7)) == 'artbox:') {  //pdfinfo documentation: https://www.xpdfreader.com/pdfinfo-man.html
+						$artbox_info = $cline;
+					}
+					if ($pagesize_info && $artbox_info) break;
+				}
+				if (!$pagesize_info) throw new \Exception('xpdf pdfinfo command for getting PDF boxes failed to find page size line.');
+				if (!$artbox_info) throw new \Exception('xpdf pdfinfo command for getting PDF boxes failed to find ArtBox line.');
+
+				if (!preg_match("/([\\d\\.]+)\\s*x\\s*([\\d\\.]+)\\s*pts/", $pagesize_info, $pagesize_match)) {
+					throw new \Exception('xpdf pdfinfo command for getting PDF boxes failed to parse page size details.');
+				}
+				if (!preg_match("/([\\d\\.]+)\\s+([\\d\\.]+)\\s+([\\d\\.]+)\\s+([\\d\\.]+)/", $artbox_info, $artbox_match)) {// NOTE: the format is "llx lly urx ury" according to https://forum.xpdfreader.com/viewtopic.php?f=3&t=41207
+					throw new \Exception('xpdf pdfinfo command for getting PDF boxes failed to parse ArtBox details.');
+				}
+				$pagesize_info = array(
+					'width' => round($pagesize_match[1], 2),
+					'height' => round($pagesize_match[2], 2),
+				);
+				$artbox_info = array(
+					'll_x' => round($artbox_match[1], 2),
+					'll_y' => round($artbox_match[2], 2),
+					'ur_x' => round($artbox_match[3], 2),
+					'ur_y' => round($artbox_match[4], 2),
+				);
+
+				// Check if ArtBox is different than page size and if so calculate the x and y offset and width and height cropping parameters on the image that will match the ArtBox
+				if ($artbox_info['ll_x'] != 0 || $artbox_info['ll_y'] != 0 || $artbox_info['ur_x'] != $pagesize_info['width'] || $artbox_info['ur_y'] != $pagesize_info['height']) {
+					$crop_to_artbox = true;
+
+					$multiply_ratio = $options['resolution'] / 72;   //from points to pixels: 841.89 pts (A4 height) / 72 points per inch * 150 dpi = 1754 px
+
+					$px_ll_x = round($artbox_info['ll_x'] * $multiply_ratio);
+					$px_ll_y = round($artbox_info['ll_y'] * $multiply_ratio);
+					$px_width = round(($artbox_info['ur_x'] - $artbox_info['ll_x']) * $multiply_ratio);
+					$px_height = round(($artbox_info['ur_y'] - $artbox_info['ll_y']) * $multiply_ratio);
+				} else {
+					$crop_to_artbox = false;
+				}
+			}
+
+
 			clearstatcache();
 			for ($i = 1; $i < 1000; $i++) {
 				$filename_src  = sprintf($image_path .'-%1$06d.png', $i);
-				$filename_dest = sprintf($image_path .'-%1$06d.jpg', $i);
-				if (file_exists($filename_src)) {
-					$coutput = array(); $returncode = 0;
-					$cmd = 'convert '. escapeshellarg($filename_src) .' -alpha remove -quality '. $options['jpg_quality'] .' '. escapeshellarg($filename_dest) .' 2>&1';  //about alpha remove: You should use -alpha remove rather than mis-use -flatten. It has the same effect, but the alpha on is faster and more memory efficient. It also works with "mogrify", where -flatten will not. Source: https://www.imagemagick.org/discourse-server/viewtopic.php?t=24048
+
+				// Exit loop when no more files exist
+				if (!file_exists($filename_src)) {
+					break;
+				}
+
+				// Crop the image to the ArtBox
+				if ($crop_to_artbox) {
+					$cmd = 'convert '. escapeshellarg($filename_src) .' -gravity SouthWest -crop '. $px_width .'x'. $px_height .'+'. $px_ll_x .'+'. $px_ll_y .' '. escapeshellarg($filename_src) .' 2>&1';
+					$coutput = array();
 					exec($cmd, $coutput, $returncode);
 
 					if (!empty($coutput)) {
-						throw new \Exception('ImageMagick command for converting PNG to JPG outputted unexpected data ((INTERNAL:'. json_encode($coutput) .'))');
+						throw new \Exception('ImageMagick command for cropping image to ArtBox outputted unexpected data ((INTERNAL:'. json_encode($coutput) .'))');
 					} elseif ($returncode != 0) {
-						throw new \Exception('ImageMagick command for converting PNG to JPG returned '. $returncode);
+						throw new \Exception('ImageMagick command for cropping image to ArtBox returned '. $returncode);
 					}
-				} else {
-					break;
+				}
+
+				// xpdf doesn't support output in jpg so in that case we do this fill-in using ImageMagick to support that
+				if ($options['output_format'] === 'jpg') {
+					$filename_dest = sprintf($image_path .'-%1$06d.jpg', $i);
+					if (file_exists($filename_src)) {
+						$coutput = array(); $returncode = 0;
+						$cmd = 'convert '. escapeshellarg($filename_src) .' -alpha remove -quality '. $options['jpg_quality'] .' '. escapeshellarg($filename_dest) .' 2>&1';  //about alpha remove: You should use -alpha remove rather than mis-use -flatten. It has the same effect, but the alpha on is faster and more memory efficient. It also works with "mogrify", where -flatten will not. Source: https://www.imagemagick.org/discourse-server/viewtopic.php?t=24048
+						exec($cmd, $coutput, $returncode);
+
+						if (!empty($coutput)) {
+							throw new \Exception('ImageMagick command for converting PNG to JPG outputted unexpected data ((INTERNAL:'. json_encode($coutput) .'))');
+						} elseif ($returncode != 0) {
+							throw new \Exception('ImageMagick command for converting PNG to JPG returned '. $returncode);
+						}
+					}
 				}
 			}
 		}
