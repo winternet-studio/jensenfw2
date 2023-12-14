938e6f399c57f4a69e3a783737830fd4a8af7ee1
Allan Jensen <info@winternet.no>
2023-12-14 23:58:52 +0100

    refactor: cleanup

--- tests/url_managerTest.php
+++ tests/url_managerTest.php
@@ -5,7 +5,7 @@ use winternet\jensenfw2\url_manager;
 final class url_managerTest extends TestCase {
 
 	public function testUrlManager() {
-		$_SERVER['DOCUMENT_ROOT'] = '/www-root/';
+		$_SERVER['DOCUMENT_ROOT'] = '/www/';
 
 		// No match
 		$_SERVER['REQUEST_URI'] = '/php/test.php?arg=45&cci=first';
@@ -16,24 +16,30 @@ final class url_managerTest extends TestCase {
 		$this->assertFalse($result);
 
 		// Has simple match
-		$_SERVER['REQUEST_URI'] = '/pdf/create';
-		$handler = new url_manager();
-		$handler->add_url('^pdf/create$', 'create.php');
-		$result = $handler->run(['return' => true]);
-		$this->assertSame('/www-root/create.php', $result);
-
 		$_SERVER['REQUEST_URI'] = '/create';
 		$handler = new url_manager();
 		$handler->add_url('^create$', 'create.php');
 		$result = $handler->run(['return' => true]);
-		$this->assertSame('/www-root/create.php', $result);
+		$this->assertSame('/www/create.php', $result);
+
+		$_SERVER['REQUEST_URI'] = '/pdf/create';
+		$handler = new url_manager();
+		$handler->add_url('^pdf/create$', 'create.php');
+		$result = $handler->run(['return' => true]);
+		$this->assertSame('/www/create.php', $result);
+
+		$_SERVER['REQUEST_URI'] = '/old_nonexisting.php';
+		$handler = new url_manager();
+		$handler->add_url('^old_nonexisting\.php$', 'new.php');
+		$result = $handler->run(['return' => true]);
+		$this->assertSame('/www/new.php', $result);
 
 		// Has simple match with query string
 		$_SERVER['REQUEST_URI'] = '/pdf/create?arg=abc';
 		$handler = new url_manager();
 		$handler->add_url('^pdf/create$', 'somefolder/create.php');
 		$result = $handler->run(['return' => true]);
-		$this->assertSame('/www-root/somefolder/create.php', $result);
+		$this->assertSame('/www/somefolder/create.php', $result);
 	}
 
 }
